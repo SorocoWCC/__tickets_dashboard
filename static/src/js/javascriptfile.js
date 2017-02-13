75d00161b38b5502b0830032dc7fa9f3dd0dcff1
Andrey Castro <a.castro.carvajal@accenture.com>
2017-02-13 15:10:17 -0600

    [Pluming] Added JS layer avoiding JS conflicts between custom and Odoo scripts

--- static/src/js/javascriptfile.js
+++ static/src/js/javascriptfile.js
@@ -1,46 +1,85 @@
 (function() {
-    function demo_alert(view) {
-        window.setTimeout(function() {
 
-            var active_view = view.action_manager.inner_widget.active_view;
-
-            
-            if (typeof(active_view) != 'undefined'){
-                
-                var controller = view.action_manager.inner_widget.views[active_view].controller;
-                var action = view.action_manager.inner_widget.action;
-                
-                console.log(controller.model);
-                
-                /*if (active_view == "form" && controller.model == "static.resource.demo"){
-                    alert('hi there!')
-                }*/
-            }
-	           //demo_alert(view);
-        }, 10000);
-    };
-
-    openerp.web.WebClient.include({
-        start: function() {
-            this._super();
-            //demo_alert(this);
-        },
-    });
-
-    jQuery(document).ready(function(e){
-
-        console.log('ready');
-
-        var Users = new openerp.Model('purchase.order');
-        
-        Users.query(['name', 'partner_id'])
-            .filter([['pago_caja', '=', 'pagado']])
-            .limit(15)
-            .all().then(function (users) {
-                console.log(users);
+    /*Starts Editable Object*/
+    var _model, _view, _controller, body, containter, ticketsTable,
+    ticketsModel = {
+        attachHandlers: function(){
+            jQuery(document).on('keydown', function(e){
+                if (e.keyCode === 27){
+                    body.toggleClass('ticketList');
+                };
             });
 
+        },
+        requestTickets: function(callback){
+            var that = this,
+            tickets = new openerp.Model('purchase.order');
+        
+            tickets.query(['name', 'partner_id'])
+                .filter([['pago_caja', '=', 'pagado']])
+                .limit(15)
+                .all().then(function(tickets) {
+                    callback(tickets);
+                });
+        },
+        updateCicle: function(){
+
+        },
+        renderUI: function(){
+            //ticketsTable = container.find('#tickets-table')
+            var tTableBody = container.find('#paid-tickets-table tbody');
+
+            this.requestTickets(function(tickets){
+                var tRows = '';
+                jQuery.each(tickets, function( index, value ) {
+                    tRows += '<tr><td>'+value.name +' - '+value.partner_id[1]+'</td></tr>';
+                });
+                console.log(tTableBody);
+                tTableBody.append(tRows);
+            });
+        },
+        _init: function(model, view, controller){
+            _model = model;
+            _view = view;
+            _controller = controller;
+
+            if (view == 'list' && controller.model == 'tickets.dashboard'){
+                this.renderUI();
+                this.attachHandlers();
+            }
+        }
+    },
+    /*Ends Editable Object*/
+
+    /*===DO NOT TOUCH===*/
+    loading = function() {
+        /*@TODO: Create load(splash) screen to show until the DOM is fully loaded*/
+    },
+    igniter = function(model, fnObj){
+        var watcher = window.setInterval(function() {
+            var view, controller, cont = $('.oe_view_manager.oe_view_manager_current .oe_view_manager_body .custom-container');
+
+            if (model && model.action_manager && model.action_manager.inner_widget  
+                && model.action_manager.inner_widget.active_view  && cont.length > 0) {
+                view = model.action_manager.inner_widget.active_view;
+                controller = model.action_manager.inner_widget.views[view].controller;
+                container = cont;
+
+                window.clearInterval(watcher);
+                fnObj._init(model, view, controller);
+            }
+        }, 2000);
+    };
+    
+    jQuery(document).ready(function(){
+        body = jQuery('body');
+
+        openerp.web.WebClient.include({
+            start: function() {
+                this._super();
+                igniter(this, ticketsModel);
+            },
+        });
     });
 
-})();
-
+})(window);
