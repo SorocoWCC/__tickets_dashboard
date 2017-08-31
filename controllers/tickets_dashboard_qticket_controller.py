5331611fd76b6cddfcdbabf2273604738fa58a2b
andreyc <andreyyec@gmail.com>
2017-08-30 20:57:42 -0600

    Added users and user data rest services

--- controllers/tickets_dashboard_qticket_controller.py
+++ controllers/tickets_dashboard_qticket_controller.py
@@ -12,13 +12,24 @@ class QticketController(http.Controller):
     def index(self, **kw):
         return "Hello, world"
 
-    @http.route('/rest/qticket/getdrafts/', auth='public')
-    def getDrafts(self, **kw):
-        return http.request.render('__tickets_dashboard.listing', {
-            'orders': http.request.env['purchase.order'].search([('state', '=', 'draft'),('state', '=', 'confirmed'), ('pago_caja', '=', 'pendiente')])
-        });
+    # Object base retrive of data using render template
+    # @http.route('/rest/qticket/orders/getdrafts/', auth='public')
+    # def getDrafts(self, **kw):
+    #     return http.request.render('__tickets_dashboards.listing', {
+    #         'orders': http.request.env['purchase.order'].search([('state', '=', 'draft'),('state', '=', 'confirmed'), ('pago_caja', '=', 'pendiente')])
+    #     });
 
-    @http.route('/rest/qticket/getdrafts/json', auth='public')
+    @http.route('/rest/qticket/orders/getdrafts/', auth='public')
     def getDraftsJson(self, **kw):
     	orders = http.request.env['purchase.order'].search_read([('state', '=', 'draft'),('state', '=', 'confirmed'), ('pago_caja', '=', 'pendiente')])
         return json.dumps(orders)
+
+    @http.route('/rest/qticket/users/getusers/', auth='public')
+    def getUsersJson(self, **kw):        
+        users = http.request.env['res.users'].search_read([('state', '=', 'active')])
+        return json.dumps(users)
+
+    @http.route('/rest/qticket/users/getuser/<string(minlength=1):username>', auth='public')
+    def getUserJson(self, username, **kw):        
+        users = http.request.env['res.users'].search_read([('login', '=', username)])
+        return json.dumps(users)
