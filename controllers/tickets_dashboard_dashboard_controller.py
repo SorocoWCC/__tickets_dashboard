815110555e9e4eae71bd31093c854bb5111cd4c8
andreyc <andreyyec@gmail.com>
2017-09-15 17:48:17 -0600

    Looping over the drafts array

--- controllers/tickets_dashboard_dashboard_controller.py
+++ controllers/tickets_dashboard_dashboard_controller.py
@@ -9,16 +9,24 @@ class DashboardController(http.Controller):
 
     @http.route('/odoo/test', type='json', auth='public', methods=['POST'], website=True)
     def getDraftsList(self, **args):
+        draftList = {}
         drafts = http.request.env['purchase.order'].search_read([('state', '=', 'draft'), ('pago_caja', '=', 'pendiente')])
         ids = args.get('ids', False)
         qticketList = ids['ids']
         
-        return json.dumps(drafts)
+        for draft in drafts:
+            # @TODO: Compare list of tickets and return ids to be added and deleted
+             draftList[draft["name"]] = {
+                "id": draft["name"],
+                "client": draft["partner_id"],
+                "ticket": draft["placa"]
+             }
 
+             _logger.info("=> Draft")
 
-        # for x in qticketList:
-        #     # @TODO: Compare list of tickets and return ids to be added and deleted
-        #     _logger.info(x)
+             _logger.info(draftList)
+
+        return json.dumps(draftList)
 
         # if not ids:
         #     Response.status = '400 Bad Request'
