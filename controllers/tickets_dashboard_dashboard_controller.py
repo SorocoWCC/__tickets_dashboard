bd5f976e06fa6280749f770f9c21cd7f9276b7a5
andreyc <andreyyec@gmail.com>
2017-09-16 22:44:38 -0600

    Odoo Drafts DiffList endpoint

--- controllers/tickets_dashboard_dashboard_controller.py
+++ controllers/tickets_dashboard_dashboard_controller.py
@@ -7,27 +7,71 @@ _logger = logging.getLogger(__name__)
 
 class DashboardController(http.Controller):
 
-    @http.route('/odoo/test', type='json', auth='public', methods=['POST'], website=True)
+    def getDraftsPristineDict(instance, odooDraftsDict):
+        cleanDict = {}
+
+        for draft in odooDraftsDict:
+            # _logger.info("=> Iteration")
+            # _logger.info(draft)
+            key = draft["name"]
+            #_logger.info("=> Index" + key)
+            cleanDict[draft["name"]] = draft
+
+        #_logger.info(cleanDict)
+        return cleanDict
+
+    def getListDIff(instance, odooDraftsDict, qTicketList):
+        diffDict = {"added":[], "removed":[]}
+        toRemoveKeyList = []
+
+        _logger.info("=> Qticket List Start:")
+
+        # Check every order from Odoo and save the 
+        # ID's of each order already loaded
+        if len(qTicketList) > 0:
+            for xKey in qTicketList:
+                for yKey in odooDraftsDict:
+                    cDraft = odooDraftsDict[yKey]
+                    if xKey == cDraft["name"]:
+                        toRemoveKeyList.append(xKey);
+
+            # Remove any orders repeated from the list retrieved by Odoo
+            for key in toRemoveKeyList:
+                del odooDraftsDict[key]
+                qTicketList.remove(key)
+                        
+        # Get the remaining Ids in the list and set them
+        # as "to remove" in Qticket
+        if len(qTicketList) > 0:
+            for nKey in qTicketList:
+                diffDict["removed"].append(nKey)
+                qTicketList.remove(nKey)
+
+        _logger.info("=====> qTicketList remaining")
+        _logger.info(qTicketList)
+
+        # Process and filter the results from Odoo 
+        # to retrieve them to Qticket
+        if len(odooDraftsDict) > 0:
+            for draftKey in odooDraftsDict:
+                currentDraft = odooDraftsDict[draftKey]
+
+                diffDict["added"].append({
+                    "id": currentDraft["name"],
+                    "client": currentDraft["partner_id"],
+                    "ticket": currentDraft["placa"]
+                 })
+
+        return diffDict;
+
+    @http.route('/rest/purchases/drafts/list', type='json', auth='public', methods=['POST'], website=True)
     def getDraftsList(self, **args):
-        draftList = {}
         drafts = http.request.env['purchase.order'].search_read([('state', '=', 'draft'), ('pago_caja', '=', 'pendiente')])
         ids = args.get('ids', False)
-        qticketList = ids['ids']
-        
-        for draft in drafts:
-            # @TODO: Compare list of tickets and return ids to be added and deleted
-             draftList[draft["name"]] = {
-                "id": draft["name"],
-                "client": draft["partner_id"],
-                "ticket": draft["placa"]
-             }
+        qTicketList = ids['ids']
 
-             _logger.info("=> Draft")
+        draftsDict = self.getDraftsPristineDict(drafts);
 
-             _logger.info(draftList)
+        updatesList = self.getListDIff(draftsDict, qTicketList);
 
-        return json.dumps(draftList)
-
-        # if not ids:
-        #     Response.status = '400 Bad Request'
-        # return '{"response": "OK"}'
+        return json.dumps(updatesList)
